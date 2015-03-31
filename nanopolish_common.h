e14e34d523add8db94aa4a5d403db6cd74488215
jsimpson <jared.simpson@gmail.com>
2015-03-31 14:26:33 -0400

    v0.2.0

--- nanopolish_common.h
+++ nanopolish_common.h
@@ -17,7 +17,7 @@
 #include "logsum.h"
 
 #define PACKAGE_NAME "nanopolish"
-#define PACKAGE_VERSION "0.1.0"
+#define PACKAGE_VERSION "0.2.0"
 #define PACKAGE_BUGREPORT "https://github.com/jts/nanopolish/issues"
 
 //
