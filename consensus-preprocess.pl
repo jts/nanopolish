6dda2b59491c8ddfb42b7ccf6183d006651877c9
Rob Egan <rsegan@lbl.gov>
2015-04-22 17:24:10 -0700

    in consensus.make   fixed namespace conflicts   added nanopolish consensus and merge steps   added clean target
    in consensus-preprocess.pl
      fixed output fofn name and contents to be same as expected in nanopolish_fast5_map.cpp
      fixed name to path to properly track with new ONT / poretools naming of reads

--- consensus-preprocess.pl
+++ consensus-preprocess.pl
@@ -1,4 +1,4 @@
-#! /usr/bin/perl
+#! /usr/bin/env perl
 #
 # Extract fast5 file names from a poretools fasta file and prepend an index to uniquify names
 #
@@ -8,7 +8,7 @@ use Getopt::Long;
 
 my $inFile  = Bio::SeqIO->new(-file => $ARGV[0], '-format' => 'Fasta');
 my $outFile = Bio::SeqIO->new(-fh => \*STDOUT, '-format' => 'Fasta');
-open(FM, ">consensus_fast5.fofn");
+open(FM, ">$ARGV[0].fast5.fofn");
 
 my $id = 0;
 
@@ -17,8 +17,10 @@ while(my $seq = $inFile->next_seq())
     my $in_name = $seq->display_id();
     my $out_name = "$id.$in_name";
     my $desc = $seq->desc();
+    my ($path) = $desc =~ /([^:\s]+\/\S+)/;
+    if (not defined $path) { $path = $desc; }
     
-    print FM "$desc\n";
+    print FM "$out_name\t$path\n";
 
     $seq->display_id($out_name);
     $outFile->write_seq($seq);
