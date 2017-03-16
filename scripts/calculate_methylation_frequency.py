2ee45754c73a344d2cdbc0007a5a7877ba45288e
Jared Simpson <jared.simpson@gmail.com>
2017-03-16 09:55:01 -0400

    improve output, calculate frequencies at non-singletons

--- scripts/calculate_methylation_frequency.py
+++ scripts/calculate_methylation_frequency.py
@@ -7,14 +7,16 @@ import argparse
 from collections import namedtuple
 
 class SiteStats:
-    def __init__(self):
+    def __init__(self, g_size, g_seq):
         self.num_reads = 0
         self.posterior_methylated = 0
         self.called_sites = 0
         self.called_sites_methylated = 0
+        self.group_size = g_size
+        self.sequence = g_seq
 
 parser = argparse.ArgumentParser( description='Calculate methylation frequency at genomic CpG sites')
-parser.add_argument('-c', '--call-threshold', type=float, required=False, default=0)
+parser.add_argument('-c', '--call-threshold', type=float, required=False, default=2.5)
 parser.add_argument('-i', '--input', type=str, required=False)
 args = parser.parse_args()
 assert(args.call_threshold is not None)
@@ -29,13 +31,10 @@ csv_reader = csv.DictReader(in_fh, delimiter='\t')
 
 for record in csv_reader:
     num_sites = int(record['num_cpgs']) 
-    # skip non-singletons for now
-    if num_sites > 1:
-        continue
     
-    key = record['chromosome'] + ":" + record['start'] + "-" + record['end']
+    key = record['chromosome'] + ":" + record['start'] + ":" + record['end']
     if key not in sites:
-        sites[key] = SiteStats()
+        sites[key] = SiteStats(num_sites, record['sequence'].rstrip())
 
     llr = float(record['log_lik_ratio'])
 
@@ -48,10 +47,11 @@ for record in csv_reader:
             sites[key].called_sites_methylated += num_sites
 
 # header
-print "\t".join(["key", "called_sites", "called_sites_methylated", "methylated_frequency"])
+print "\t".join(["chromosome", "start", "end", "num_cpgs_in_group", "called_sites", "called_sites_methylated", "methylated_frequency", "group_sequence"])
 
 for key in sites:
     if sites[key].called_sites > 0:
+        (c, s, e) = key.split(":")
         f = float(sites[key].called_sites_methylated) / sites[key].called_sites
-        print "\t".join([str(x) for x in [key, sites[key].called_sites, sites[key].called_sites_methylated, f]])
+        print "\t".join([str(x) for x in [c, s, e, sites[key].group_size, sites[key].called_sites, sites[key].called_sites_methylated, f, sites[key].sequence]])
 
