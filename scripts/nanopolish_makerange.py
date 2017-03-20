33b7e9371305c4171594c21c154cd5724ea013cb
Jared Simpson <jared.simpson@gmail.com>
2017-03-20 15:39:49 -0400

    allow segment and overlap be specified as a parameter

--- scripts/nanopolish_makerange.py
+++ scripts/nanopolish_makerange.py
@@ -1,10 +1,19 @@
 import sys
+import argparse
 from Bio import SeqIO
 
-recs = [ (rec.name, len(rec.seq)) for rec in SeqIO.parse(open(sys.argv[1]), "fasta")]
+parser = argparse.ArgumentParser(description='Partition a genome into a set of overlapping segments')
+parser.add_argument('--segment-length', type=int, default=50000)
+parser.add_argument('--overlap-length', type=int, default=200)
+args, extra = parser.parse_known_args()
+if len(extra) != 1:
+    sys.stderr.write("Error: a genome file is expected\n")
+filename = extra[0]
 
-SEGMENT_LENGTH = 50000
-OVERLAP_LENGTH = 200
+recs = [ (rec.name, len(rec.seq)) for rec in SeqIO.parse(open(filename), "fasta")]
+
+SEGMENT_LENGTH = args.segment_length
+OVERLAP_LENGTH = args.overlap_length
 
 for name, length in recs:
     n_segments = (length / SEGMENT_LENGTH) + 1
