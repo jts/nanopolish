90885693ed3296fab528cefeae4138ab326dc0dc
jopineda <joannaa.pineda@gmail.com>
2017-11-14 15:00:13 -0500

    Added docs folder

---
+++ docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to Nanopolish\u2019s documentation!"],titleterms:{document:0,indic:0,nanopolish:0,tabl:0,welcom:0}})
