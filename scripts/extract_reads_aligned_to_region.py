jts/nanopolish

commit ee6b01dfaa59a8db30c785b329bda93858d76f64

Use SimpleFastaParser and FastqGeneralIterator
These are the low-level string based parsers underlying the
high level object-heavy SeqIO.parse(...) API.

scripts/extract_reads_aligned_to_region.py
@@ -8,7 +8,8 @@ in draft genome assembly.
 from __future__ import print_function
 
 try:
-	from Bio import SeqIO
+	from Bio.SeqIO.FastaIO import SimpleFastaParser
+        from Bio.SeqIO.QualityIO import FastqGeneralIterator
 	import pysam
 	import argparse
 	import subprocess
@@ -146,27 +147,27 @@ def main():
 		if ".gz" in file_type:
 			with gzip.open(o_fa, "rt") as fin:
 				if "fasta.gz" in file_type:
-					for record in SeqIO.parse(fin, "fasta"):
+					for title, seq in SimpleFastaParser(handle):
+						name = title.split(None, 1)[0]
 						if record.id in region_read_ids:
-							fout.write(">" + record.id + "\n")
-							fout.write(str(record.seq) + "\n")
+							fout.write(">%s\n%s\n" % (name, seq))
 				elif "fastq.gz" in file_type:
-					for record in SeqIO.parse(fin, "fastq"):
+					for title, seq, qual in FastqGeneralIterator(handle):
+						name = title.split(None, 1)[0]
 						if record.id in region_read_ids:
-							fout.write(">" + record.id + "\n")
-							fout.write(str(record.seq) + "\n")
+							fout.write(">%s\n%s\n" % (name, seq))
 		else:
 			with open(o_fa, "rt") as fin:
 				if "fasta" in file_type:
-					for record in SeqIO.parse(fin, "fasta"):
+					for title, seq in SimpleFastaParser(handle):
+						name = title.split(None, 1)[0]
 						if record.id in region_read_ids:
-							fout.write(">" + record.id + "\n")
-							fout.write(str(record.seq) + "\n")
+							fout.write(">%s\n%s\n" % (name, seq))
 				elif "fastq" in file_type:
-					for record in SeqIO.parse(fin, "fastq"):
+					for title, seq, qual in FastqGeneralIterator(handle):
+						name = title.split(None, 1)[0]
 						if record.id in region_read_ids:
-							fout.write(">" + record.id + "\n")
-							fout.write(str(record.seq) + "\n")
+							fout.write(">%s\n%s\n" % (name, seq))
 
 	# --------------------------------------------------------
 	# PART 7: 	Let's get to tarring

commit e31834a0dafb9f1d3b7daaa19b3eadb32c52c1ad

Fix filehandle and name references
Commit ee6b01dfaa59a8db30c785b329bda93858d76f64 broke this script as it introduced new variable names but didn't change the old usages. This commit fixes the names.

scripts/extract_reads_aligned_to_region.py
@@ -147,26 +147,26 @@ def main():
 		if ".gz" in file_type:
 			with gzip.open(o_fa, "rt") as fin:
 				if "fasta.gz" in file_type:
-					for title, seq in SimpleFastaParser(handle):
+					for title, seq in SimpleFastaParser(fin):
 						name = title.split(None, 1)[0]
-						if record.id in region_read_ids:
+						if name in region_read_ids:
 							fout.write(">%s\n%s\n" % (name, seq))
 				elif "fastq.gz" in file_type:
-					for title, seq, qual in FastqGeneralIterator(handle):
+					for title, seq, qual in FastqGeneralIterator(fin):
 						name = title.split(None, 1)[0]
-						if record.id in region_read_ids:
+						if name in region_read_ids:
 							fout.write(">%s\n%s\n" % (name, seq))
 		else:
 			with open(o_fa, "rt") as fin:
 				if "fasta" in file_type:
-					for title, seq in SimpleFastaParser(handle):
+					for title, seq in SimpleFastaParser(fin):
 						name = title.split(None, 1)[0]
-						if record.id in region_read_ids:
+						if name in region_read_ids:
 							fout.write(">%s\n%s\n" % (name, seq))
 				elif "fastq" in file_type:
-					for title, seq, qual in FastqGeneralIterator(handle):
+					for title, seq, qual in FastqGeneralIterator(fin):
 						name = title.split(None, 1)[0]
-						if record.id in region_read_ids:
+						if name in region_read_ids:
 							fout.write(">%s\n%s\n" % (name, seq))
 
 	# --------------------------------------------------------